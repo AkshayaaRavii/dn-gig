04cad8e80130bb2cbe42c15545b6c96cca510958
Akshaya Ravi <akshaya@appveen.com>
2023-06-05 22:46:10 +0530

    Adding Widgets

--- src/app/main-container/canvas/canvas.component.ts
+++ src/app/main-container/canvas/canvas.component.ts
@@ -1,11 +1,24 @@
-import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
-
+import {
+  Component,
+  ComponentFactoryResolver,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
+import { ButtonComponent } from 'src/app/widgets/button/button.component';
+import { InputComponent } from 'src/app/widgets/input/input.component';
+import { TypographyComponent } from 'src/app/widgets/typography/typography.component';
+@ViewChild('container', { read: ViewContainerRef })
 @Component({
   selector: 'app-canvas',
   templateUrl: './canvas.component.html',
   styleUrls: ['./canvas.component.scss'],
 })
 export class CanvasComponent implements OnInit {
+  @ViewChild('container', { read: ViewContainerRef })
+  container!: ViewContainerRef;
+
   constructor(private componentFactoryResolver: ComponentFactoryResolver) {}
 
   ngOnInit(): void {}
@@ -17,24 +30,24 @@ export class CanvasComponent implements OnInit {
     event.preventDefault();
     const x1 = event.clientX;
     const y1 = event.clientY;
-    // if (data) {
-    //   const componentFactory =
-    //     this.componentFactoryResolver.resolveComponentFactory(
-    //       data === 'Button'
-    //         ? ButtonComponent
-    //         : data === 'Input'
-    //         ? InputComponent
-    //         : data === 'Cards'
-    //         ? ContainerComponent
-    //         : CustomComponent
-    //     );
-    //   const componentRef = this.container.createComponent(componentFactory);
-    //   componentRef.location.nativeElement.style.position = 'absolute';
-    //   componentRef.location.nativeElement.style.left = x1 + 'px';
-    //   componentRef.location.nativeElement.style.top = y1 + 'px';
-    //   componentRef.location.nativeElement.id = data;
-    //   event.preventDefault();
-    // } else {
+    if (data) {
+      const componentFactory =
+        this.componentFactoryResolver.resolveComponentFactory(
+          data === 'Button'
+            ? ButtonComponent
+            : data === 'Input'
+            ? InputComponent
+            : // : data === 'Cards'
+              // ? ContainerComponent
+              TypographyComponent
+        );
+      const componentRef = this.container.createComponent(componentFactory);
+      componentRef.location.nativeElement.style.position = 'absolute';
+      componentRef.location.nativeElement.style.left = x1 + 'px';
+      componentRef.location.nativeElement.style.top = y1 + 'px';
+      componentRef.location.nativeElement.id = data;
+      event.preventDefault();
+    } // else {
     //   const draggedElement = data && document.getElementById(data);
     //   const dropzone = event.target as HTMLElement;
     //   const x = event.clientX - dropzone.offsetLeft;
